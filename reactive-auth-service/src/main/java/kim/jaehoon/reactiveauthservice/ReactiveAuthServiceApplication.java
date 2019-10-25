c17239b4418748807d60f5d1f243d0fb3f0b5448
Jaehoon Kim <by09115@outlook.kr>
2019-10-25 23:44:58 +0900

    ADD: new project structure

---
+++ reactive-auth-service/src/main/java/kim/jaehoon/reactiveauthservice/ReactiveAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package kim.jaehoon.reactiveauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveAuthServiceApplication.class, args);
+    }
+
+}
---
+++ reactive-auth-service/src/test/java/kim/jaehoon/reactiveauthservice/ReactiveAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.jaehoon.reactiveauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
